9086e547a95e89d6b96460e69208e33b5372d9fb
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-02-11 21:04:06 +0600

    Update My First Sorting Problem-codeforces.cpp

--- My First Sorting Problem-codeforces.cpp
+++ My First Sorting Problem-codeforces.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
 
+int main()
+{   
+   ll t,a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<min(a,b)<<" "<<max(a,b)<<endl;
+        
+}
+}
